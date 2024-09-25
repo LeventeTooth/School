d2fa2346e6a859d09ba9bcd41fe440a72f685fd4
Levi <toth.levente.balint@gmail.com>
2024-09-25 20:31:46 +0200

    mukodoWeatherApi

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let locations = {
-    "bp" : "latitude=19.040236&longitude=47.497913",
+    "bp" : "latitude=47.497913&longitude=19.040236",
+    "ch" : "latitude=41.878113&longitude=-87.629799",
     "sze" : "latitude=47.673470&longitude=19.072540"
 }
 
-function click(){
-    alert('aa')
+function runTemp(event, a){
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function (){
-        document.getElementById('aaaa').innerHTML=JSON.parse(this.responseText).current.temperature_2m+" Fok van "+a+'n'
         //console.log(JSON.parse(this.responseText).current.temperature_2m);
+        document.getElementById("homerseklet").innerHTML=`Jelnleg ${JSON.parse(this.responseText).current.temperature_2m} fok van ${event.target.textContent}-en (-ban)`
     }
     xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?${a}&current=temperature_2m`)
     xhttp.send()
