95cb7799cb5430f69b0f9cf9f9fcfd64e2440d98
LeventeTooth <toth.levente.balint@blathy.info>
2024-09-26 08:39:40 +0200

    oneWeekEarlyerTemp

--- main.js
+++ main.js
@@ -8,8 +8,25 @@ function runTemp(event, a){
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function (){
         //console.log(JSON.parse(this.responseText).current.temperature_2m);
-        document.getElementById("homerseklet").innerHTML=`Jelnleg ${JSON.parse(this.responseText).current.temperature_2m} fok van ${event.target.textContent}-en (-ban)`
+        document.getElementById("homerseklet").innerHTML=`Jelnleg ${JSON.parse(this.responseText).current.temperature_2m} fok van ${event.target.textContent}-en (-ban).`
     }
     xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?${a}&current=temperature_2m`)
+    xhttp.send()
+    sevenDaysAgo(a)
+}
+function sevenDaysAgo(a){
+    const xhttp = new XMLHttpRequest()
+    xhttp.onload = function (){
+        let times = JSON.parse(this.responseText).hourly.time
+        let temps = JSON.parse(this.responseText).hourly.temperature_2m
+        //console.log(times);
+        times.forEach(function callback(value, index) {
+            if(value=="2024-09-19T12:00"){
+                document.getElementById("homerseklet").innerHTML+=`${temps[index]} fok volt 1 hete 12:00-kor`
+            }
+            //console.log(`${index}: ${value}`);
+          });
+    }
+    xhttp.open("GET", `https://api.open-meteo.com/v1/forecast?${a}&past_days=10&hourly=temperature_2m`)
     xhttp.send()
 }
